4f0ddc078a66bfec338f3e205b7082f170f3badb
edson <edson.rodgon@gmail.com>
2023-01-17 17:23:21 -0300

    just a test

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  resources :secret_menu_items, only: [:index, :show, :create, :update, :destroy]
+  resources :secret_menu_items, only: [:index, :show, c:create, :update, :destroy]
 end
+#this is a comment
